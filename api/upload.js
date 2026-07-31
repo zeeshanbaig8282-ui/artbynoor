a0b695a9318ceab1364a1331981b6faffc18c87e
zeeshanbaig8282-ui <zeeshanbaig8282@gmail.com>
2026-07-31 21:49:03 +0500

    Add files via upload

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,6 @@
 import { put } from '@vercel/blob';
 
-const ALLOWED_CATEGORIES = ['crochet', 'painting', 'crafts', 'mehndi', 'jewelry', 'charms'];
+const ALLOWED_CATEGORIES = ['slideshow', 'crochet', 'painting', 'crafts', 'mehndi', 'jewelry', 'charms'];
 const MAX_BYTES = 4.2 * 1024 * 1024; // stay under Vercel's request body limit
 
 export default async function handler(req, res) {
